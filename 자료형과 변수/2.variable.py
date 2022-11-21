1da7a53dc7ce020f04b6002fe2c3bd7c7c4e47fb
rudqls007 <rudqls007@gmail.com>
2022-11-21 11:01:30 +0900

    변수

---
+++ 자료형과 변수/2.variable.py
@@ -0,0 +1,17 @@
+# 변수
+# 변수 이름 = 데이터
+# 변수 이름 짓는 규칙 - 문자부터 시작해야 한다.
+#                  - 대소문자 구분한다.
+#                  - _로 시작할 수 있다.
+
+name = "마스터 이"
+level = 5
+health = 800
+attack = 90
+print(name, attack, level, health, )
+
+# 변수에 저장된 데이터 변경하기
+level = level + 1           # 5 + 1
+health = health + 50        # 800 + 50
+attack = attack + 10        # 90 + 10
+print(name, level, health, attack)
